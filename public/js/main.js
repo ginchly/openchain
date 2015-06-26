39b9d7f1a40577572b4f1bc640ecb921f5b84c87
Graham Hinchly <graham.hinchly@ft.com>
2015-06-26 10:34:15 +0100

    added brand data filtering

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 // Simple list
 Sortable.create(simpleList, { 
 	animation: 300,
-	handle: ".main-text"
+	handle: ".main-text",
+    onEnd: updateOrder
  });
 
 if ('addEventListener' in document) {
@@ -18,6 +19,8 @@ var restartHtml = '<img src="img/icon-restart.svg" height=40 />';
 var nextHtml = '<img src="img/icon-next.svg" height=40 />';
 var prevHtmlDisabled = '<img src="img/icon-prev-disabled.svg" height=40 />'
 
+		var hashtagList = ""
+
 // Function to change the content of t2
 function nextPage() {
 	ga('send', 'event', 'button', 'click', 'next-page-or-restart');
@@ -27,6 +30,20 @@ function nextPage() {
 
 	if (currentPage === (maxPage -1)) {
 		document.getElementById("oc-nav-next").innerHTML = restartHtml;
+		orderBrands()
+		// finishing data
+		var listHTML = "";
+
+		for (var i = 0; i < 9; i++) {
+			var index = i + 1;
+			listHTML = listHTML + "<h5>" + index + ". " + brandData[i].name + " (" + brandData[i].score + ")</h5>";
+			if (i<3) {
+				hashtagList = hashtagList + " %23" + brandData[i].name.replace(" ", "");
+			}
+
+		};
+		document.getElementById("orderedBrands").innerHTML = listHTML;
+		document.getElementById("tweeter").innerHTML = '<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Just checked best brands that match my sustainability choices on http://openchain.co/'  + hashtagList + ' %23sustainability" data-size="large"><button type="button" class="btn btn-primary btn-lg oc-tweet-btn">Tweet this</button></a>'
 	}
 	// Must be restart
 	if (currentPage === maxPage) {
@@ -97,6 +114,20 @@ for (var i = 0; i < factors.length; i++) {
 	document.getElementById("factor-" + factors[i] + "-unacceptable").addEventListener("click", selectFactor, false)
 };
 
+var unacceptableFactors = [];
+
+var orderedFactors = factors;
+var weighting = [2, 2,2,1,0.0,0.0,0.0]
+
+function updateOrder() {
+	orderedFactors = [];
+	var orderedElements = document.getElementsByClassName("list-group-item-orderable")
+	for (var i = 0; i < orderedElements.length; i++) {
+		orderedFactors[i] = orderedElements[i].id.replace("factor-", "")
+	};
+	console.log(orderedFactors)
+}
+
 function showFactorInfo() {
 	ga('send', 'event', 'list-item', 'click', 'factor-info-toggled');
 	var id, el;
@@ -119,13 +150,79 @@ function showFactorInfo() {
 }
 
 function selectFactor() {
+	var factor = event.target.textContent.toLowerCase()
 	ga('send', 'event', 'list-item', 'click', 'select-toggled');
 	el = event.target
 	if (el.className.indexOf("selected") === -1) {
 			// remove expanded to collapse all others first
 		el.className = el.className + " selected"
+		unacceptableFactors.push(factor) 
 	} else {
 		el.className = "main-text"
+		var index = unacceptableFactors.indexOf(factor);
+		if (index > -1) {
+		    unacceptableFactors.splice(index, 1);
+		}
 	}
+	console.log(unacceptableFactors);
 }
 
+var desirableBrandList = ["Unilever","Nestlé","Coca Cola","PepsiCo","Mars","Mondelez", "Kellogg's","Danone","General Mills","Associated British Foods plc"]
+function filterBrands() {
+	for (var i = 0; i < brandData.length; i++) {
+		var brand = brandData[i]
+		for (var j = 0; j < unacceptableFactors.length; j++) {
+			var factor = unacceptableFactors[j]
+			if (brand[factor] <= 3) {
+				var index = desirableBrandList.indexOf(brand.name);
+				if (index > -1) {
+				    desirableBrandList.splice(index, 1);
+				}
+			}
+		};
+	};
+	console.log(desirableBrandList);
+}
+
+var sortedBrandList = []
+function orderBrands() {
+	filterBrands();
+	for (var i = 0; i < brandData.length; i++) {
+		var brandScore = 0;
+		var brand = brandData[i]
+		for (var j = 0; j < orderedFactors.length; j++) {
+			var factor = orderedFactors[j]
+			brandScore = brandScore + (brand[factor] * weighting[j])
+		};
+		if (desirableBrandList.indexOf(brand.name) === -1) {
+			brandScore = 0;
+		}
+		updateScore(brand, brandScore)
+	};
+	brandData.sort(compare);
+	console.log(brandData);
+}
+
+function updateScore(brand, score) {
+	var thisScore = score;
+	brand.score = Math.round(thisScore);
+}
+
+function compare(a,b) {
+  if (a.score < b.score)
+    return 1;
+  if (a.score > b.score)
+    return -1;
+  return 0;
+}
+
+var brandData = [{"name": "Unilever", "land": 7, "women": 5, "farmers": 8, "workers": 8, "climate": 9, "transparency": 7, "water": 6},
+{"name": "Nestlé", "land": 8, "women": 5, "farmers": 7, "workers": 6, "climate": 8, "transparency": 7, "water": 7},
+{"name": "Coca Cola", "land": 8, "women": 6, "farmers": 2, "workers": 6, "climate": 6, "transparency": 5, "water": 5},
+{"name": "PepsiCo", "land": 7, "women": 2, "farmers": 2, "workers": 3, "climate": 6, "transparency": 5, "water": 5},
+{"name": "Mars", "land": 2, "women": 5, "farmers": 4, "workers": 4, "climate": 6, "transparency": 4, "water": 3},
+{"name": "Mondelez", "land": 3, "women": 6, "farmers": 4, "workers": 3, "climate": 4, "transparency": 4, "water": 2},
+{"name": "Kellogg's", "land": 2, "women": 3, "farmers": 2, "workers": 2, "climate": 7, "transparency": 5, "water": 3},
+{"name": "Danone", "land": 2, "women": 1, "farmers": 2, "workers": 3, "climate": 6, "transparency": 5, "water": 3},
+{"name": "General Mills", "land": 2, "women": 2, "farmers": 2, "workers": 2, "climate": 5, "transparency": 4, "water": 5},
+{"name": "Associated British Foods plc", "land": 3, "women": 2, "farmers": 3, "workers": 4, "climate": 4, "transparency": 3, "water": 2}];
